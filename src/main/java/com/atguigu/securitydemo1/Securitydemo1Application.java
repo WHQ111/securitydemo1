e8726360f6ab8a1b8c69c3598f79e49096688dcf
qq876043667 <876043667@qq.com>
2021-06-29 17:17:23 +0800

    master version0629

--- src/main/java/com/atguigu/securitydemo1/Securitydemo1Application.java
+++ src/main/java/com/atguigu/securitydemo1/Securitydemo1Application.java
@@ -1,8 +1,13 @@
 package com.atguigu.securitydemo1;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Administrator
+ */
+@MapperScan("com.atguigu.securitydemo1.mapper")
 @SpringBootApplication
 public class Securitydemo1Application {
 
