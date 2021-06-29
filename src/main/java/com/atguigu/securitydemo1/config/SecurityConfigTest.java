b9a3b72996075545b02016a6cf594aee485f2cc3
qq876043667 <876043667@qq.com>
2021-06-29 17:31:42 +0800

    master version1.0

--- src/main/java/com/atguigu/securitydemo1/config/SecurityConfigTest.java
+++ src/main/java/com/atguigu/securitydemo1/config/SecurityConfigTest.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,5 +27,18 @@ public class SecurityConfigTest extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/login.html") //登录页面设置
+                .loginProcessingUrl("/user/login") //登录访问路径
+                .defaultSuccessUrl("/test/index").permitAll()
+                .and().authorizeRequests()
+                    .antMatchers("/", "/test/add", "/user/login").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
+
     }
 }
